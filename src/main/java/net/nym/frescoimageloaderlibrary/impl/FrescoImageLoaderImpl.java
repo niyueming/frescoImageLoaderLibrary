f283d087fc64912604f8cf1808501955c3a4df80
niyueming <niyueming@163.com>
2017-02-04 11:15:31 +0800

    View

--- src/main/java/net/nym/frescoimageloaderlibrary/impl/FrescoImageLoaderImpl.java
+++ src/main/java/net/nym/frescoimageloaderlibrary/impl/FrescoImageLoaderImpl.java
@@ -20,10 +20,8 @@ import android.support.annotation.ColorRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
-import com.facebook.drawee.backends.pipeline.PipelineDraweeController;
 import com.facebook.drawee.backends.pipeline.PipelineDraweeControllerBuilder;
 import com.facebook.drawee.controller.BaseControllerListener;
 import com.facebook.drawee.controller.ControllerListener;
